37b482af73a7f290ebb29b97dd77890a3f2be11f
XxIntensionxX <intension1992@icloud.com>
2016-03-26 00:33:09 +0100

    123

---
+++ sql/ice/Boss_change_rewards.sql
@@ -0,0 +1,136 @@
+-- Change all Boss Rewards 
+
+-- Utgarde Keep/burg-utgarde
+-- Prinz Keleseth 23953
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30748;
+-- Skarvald der Konstrukteur 24200
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31679;
+-- Dalronn der Aufseher 24201
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31656;
+-- Ingvar der Brandschatzer 23954
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31673;
+
+-- Azjol-Nerub
+-- Krik'thir der Torwächter 28684
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31612;
+-- Hadronox 28921
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31611;
+-- Anub'arak 29120
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31610;
+
+-- Ahn'kahet
+-- Urahne Nadox 29309
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31456;
+-- Prinz Taldaram 29308
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31469;
+-- Amanitar 30258
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31463;
+-- Jedoga Schattensucher 29310
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31465;
+-- Herold Volazj 29311
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31464;
+
+-- Feste Drak'Tharon 
+-- Trollgrind 26630
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31362;
+-- Novos der Beschwörer 26631
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31350;
+-- König Dred 27483
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31349;
+-- Der Prophet Tharon'ja 26632
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31360;
+
+-- The Violet Hold
+-- Erekem 29315
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31507;
+-- Cyanigosa 31134
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31506;
+-- Ichoron 29313
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31508;
+-- Lavanthor 29312
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31509;
+-- Moragg 29316
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31510;
+-- Xevozz 29266
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31511;
+-- Zuramat the Obliterator 29314
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31512;
+
+-- Gundrak
+-- Drakkari Colossus 29307
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31365;
+-- Eck the Ferocious 29932
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=29932;
+-- Gal'darah 29306
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31368;
+-- Moorabi 29305
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30530;
+-- Slad'ran 29304 
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31370;
+
+-- Halls of Stone
+-- Das Tribunal des Zeitalters 28234
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=28234;
+-- Krystallus 27977
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31381;
+-- Maiden of Grief 27975
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31384;
+-- Sjonnir The Ironshaper 27978
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31386;
+
+
+-- Utgarde Pinnacle
+-- Gortok Palehoof 26687
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30774;
+-- King Ymiron 26861
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30788;
+-- Skadi the Ruthless 26693
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30807;
+-- Svala Sorrowgrave 26668
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30810;
+
+-- The Oculus
+-- Drakos the Interrogator 27654
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31558;
+-- Ley-Guardian Eregos 27656
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31561;
+-- Mage-Lord Urom 27655
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31560;
+-- Varos Cloudstrider 27447
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31559;
+
+-- The Nexus
+-- Anomalus 26763
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30529;
+-- Commander Kolurg 26798
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30397;
+-- Commander Stoutbeard 26796
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30398;
+-- Grand Magus Telestra 26731
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30510;
+-- Keristrasza 26723
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30540;
+-- Ormorok the Tree-Shaper 26794
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=30532;
+
+-- The Culling of Stratholme
+-- Chrono-Lord Epoch 26532
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31215;
+-- Infinite Corruptor 32273
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=32313;
+-- Mal'Ganis 26533
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31217;
+-- Meathook 26529
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31211;
+-- Salramm the Fleshcrafter 26530
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31212;
+
+-- Halls of Lightning
+-- General Bjarngrim 28586
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31533;
+-- Ionar 28546
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31537;
+-- Loken 28923
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31538;
+-- Volkhan 28587
+UPDATE `creature_loot_template` SET `Item`=40752 WHERE  `Item`IN(40753,47241,49426) AND `Entry`=31536;
